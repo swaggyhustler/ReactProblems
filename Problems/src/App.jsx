cb945c586aca1058c9611e50aaaf888df1db1d33
Chandu_Kommineni <chandukommineni63@gmail.com>
2024-07-01 09:54:42 +0530

    folder structure updated

---
+++ Problems/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import UserInput from "./problems/showuserinputinrealtime/UserInput";
+const App = () => {
+  return <div>
+        <UserInput/>
+    </div>;
+};
+
+export default App;
---
+++ Problems/src/problems/counterapp/Counter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useState } from "react";
+import "./counter.css"
+const Counter = () => {
+    const [counter,setCounter]=useState(0)
+    return(
+      <div className="container">
+  
+        <div className="counter">{counter}</div>
+        <div className="buttons">
+           <button onClick={()=>setCounter(counter+1)}>Increment</button>
+           <button onClick={()=>setCounter(counter-1)}>Decrement</button>
+          <button onClick={()=>setCounter(0)}>Reset</button>
+        </div>
+  
+  
+      </div>
+    )
+};
+
+export default Counter;
